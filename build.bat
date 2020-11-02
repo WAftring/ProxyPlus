d94cee2758da9bc38aff0604fc58621edcd9ebbf
WAftring <william.aftring@outlook.com>
2020-11-02 13:39:21 -0500

    Clarifying build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 if [%1] == [] (
-	@echo Provide prod or debug
+	@echo To compile this application with debug run "build.bat debug"
+	@echo To compile this application run "build.bat production"
 	exit /B 1
 )
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
@@ -12,6 +13,6 @@ if %1==debug (
 	cd ..
 	copy example.ini build\config.ini
 )
-if %1==prod (
+if %1==production (
 	cl /FC /EHsc /I..\src /MD ..\src\*.c /link kernel32.lib user32.lib advapi32.lib /out:"ProxyPlus.exe"
 )
